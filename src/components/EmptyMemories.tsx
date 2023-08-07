f68fc21718d6e3c4eefe6d8db89fa24080fe0fba
krasny <lookjota@gmail.com>
2023-08-07 16:04:39 -0300

    Layout de listagem / buscando memorias da API

---
+++ src/components/EmptyMemories.tsx
@@ -0,0 +1,10 @@
+export function EmptyMemories() {
+  return (
+    <div className="flex p-16 flex-1 items-center justify-center">
+      <p className="w-[360px] text-center leading-relaxed">
+        Voce ainda nao registrou nenhuma lembranca, comece a {' '}
+        <a href="" className="underline hover:text-gray-50">criar agora</a> !
+      </p>
+    </div>
+  )
+}
